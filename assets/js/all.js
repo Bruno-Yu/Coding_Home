b8b62c5ac7f63ed65860abfda2ef70cfa817d9b0
Bruno Yu <jackhellowin@gmail.com>
2022-08-15 11:56:37 +0800

    Update 2022-08-15T03:55:57.239Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,7 @@
 $(function () {
   console.log("Hello Bootstrap5");
 });
+var elem = document.querySelector('input[name="foo"]');
+var datepicker = new Datepicker(elem, {// ...options
+});
 //# sourceMappingURL=all.js.map
