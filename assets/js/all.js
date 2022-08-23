62929532a89d9dd400c1562051c0553a6d93932b
Bruno Yu <jackhellowin@gmail.com>
2022-08-23 15:24:32 +0800

    Update 2022-08-23T07:24:28.305Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,8 +2,22 @@
 
 $(function () {
   console.log("Hello Bootstrap5");
-});
-var elem = document.querySelector('input[name="foo"]');
-var datepicker = new Datepicker(elem, {// ...options
+  var date = document.querySelector('#date');
+  var datepicker = new Datepicker(date, {
+    language: 'zh-TW'
+  });
+  var swiper = new Swiper(".reviews__swiper", {
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
+    },
+    navigation: {
+      nextEl: ".function__next",
+      prevEl: ".function__prev"
+    },
+    pagination: {
+      el: ".function__pagination"
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
