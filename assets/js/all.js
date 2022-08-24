8576f663f5484c2cc9d4ed489af7f658a24d8065
Bruno Yu <jackhellowin@gmail.com>
2022-08-24 14:03:06 +0800

    Update 2022-08-24T06:02:57.616Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,23 +1,32 @@
 "use strict";
 
 $(function () {
-  console.log("Hello Bootstrap5");
-  var date = document.querySelector('#date');
-  var datepicker = new Datepicker(date, {
-    language: 'zh-TW'
-  });
-  var swiper = new Swiper(".reviews__swiper", {
+  console.log("Hello Bootstrap5"); // var swiperTestimonial = new Swiper(".swiper--testimonial", {
+  //   navigation: {
+  //     nextEl: ".function__next",
+  //     prevEl: ".function__prev",
+  //   },
+  //   pagination: {
+  //     el: ".function__pagination",
+  //   },
+  // });
+
+  var reviewsSwiper = new Swiper(".reviews__swiper", {
     effect: "fade",
     fadeEffect: {
-      crossFade: true
+      crossFade: true,
     },
     navigation: {
       nextEl: ".function__next",
-      prevEl: ".function__prev"
+      prevEl: ".function__prev",
     },
     pagination: {
-      el: ".function__pagination"
-    }
+      el: ".function__pagination",
+    },
+  });
+  var date = document.querySelector("#date");
+  var datepicker = new Datepicker(date, {
+    language: "zh-TW",
   });
 });
 //# sourceMappingURL=all.js.map
